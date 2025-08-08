6b9b3de998f5aaec39f297cd0f08ad8f98042d18
Pedro Renan <pedroviana15094@gmail.com>
2025-08-08 12:33:03 -0300

    feat: add entitie User

---
+++ src/main/java/com/github/pedrorenan/pr_ecommerce/entities/User.java
@@ -0,0 +1,21 @@
+package com.github.pedrorenan.pr_ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDay;
+    private String password;
+
+}

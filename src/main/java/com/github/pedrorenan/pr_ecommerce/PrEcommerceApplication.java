0e46c26dc36f4b00a2570ffd3312c8d39da67f42
Pedro Renan <pedroviana15094@gmail.com>
2025-08-08 12:21:56 -0300

    feat: Initial project setup

---
+++ src/main/java/com/github/pedrorenan/pr_ecommerce/PrEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrorenan.pr_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pedrorenan/pr_ecommerce/PrEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedrorenan.pr_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

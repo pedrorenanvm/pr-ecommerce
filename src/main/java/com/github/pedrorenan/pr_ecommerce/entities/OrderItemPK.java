8b455b09247048fdbcf525dc32c022a781bc9f9a
Pedro Renan <pedroviana15094@gmail.com>
2025-08-09 16:56:59 -0300

    feat: add OrderItem and OrderItemPK

---
+++ src/main/java/com/github/pedrorenan/pr_ecommerce/entities/OrderItemPK.java
@@ -0,0 +1,20 @@
+package com.github.pedrorenan.pr_ecommerce.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Embeddable
+@Data
+public class OrderItemPK {
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+}

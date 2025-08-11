5585509a8db7e814a0ee11cc403cb1034d1c2817
Pedro Renan <pedroviana15094@gmail.com>
2025-08-11 12:56:56 -0300

    feat: add ProductController with findByID and findAll with Pagination

---
+++ src/main/java/com/github/pedrorenan/pr_ecommerce/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package com.github.pedrorenan.pr_ecommerce.controllers;
+
+import com.github.pedrorenan.pr_ecommerce.dtos.ProductDTO;
+import com.github.pedrorenan.pr_ecommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
+    }
+
+}

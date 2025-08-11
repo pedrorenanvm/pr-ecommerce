a2348cf7ae0e7a79da8ae8a3aeb5b2910f79f8a2
Pedro Renan <pedroviana15094@gmail.com>
2025-08-11 12:55:37 -0300

    feat: add ProductService with findById and findAll methods

---
+++ src/main/java/com/github/pedrorenan/pr_ecommerce/services/ProductService.java
@@ -0,0 +1,30 @@
+package com.github.pedrorenan.pr_ecommerce.services;
+
+import com.github.pedrorenan.pr_ecommerce.dtos.ProductDTO;
+import com.github.pedrorenan.pr_ecommerce.entities.Product;
+import com.github.pedrorenan.pr_ecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Product product = productRepository.findById(id).get();
+        return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(ProductDTO::new);
+    }
+}
